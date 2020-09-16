82274b3ba2382744dad21a27b7157c4363714029
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-16 12:53:11 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
+char a
+int b
+long c
+float d
 int main(void)
 {
-	printf("Size of a char: %i byte(S) \n", sizeof(char *));
-	printf("Size of a int: %i byte(S) \n", sizeof(int *));
-	printf("Size of a long int: %i byte(S) \n", sizeof(long int *));
-	printf("Size of a float: %i byte(S) \n", sizeof(float *));
+	printf("Size of a char: %i byte(S) \n", sizeof(a));
+	printf("Size of a int: %i byte(S) \n", sizeof(b));
+	printf("Size of a long int: %i byte(S) \n", sizeof(c));
+	printf("Size of a float: %i byte(S) \n", sizeof(d));
 	return (0);
 }
