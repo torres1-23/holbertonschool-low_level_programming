2e0440f1b359e7d603816b8e606890a13d4847cc
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-16 13:05:05 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	char a;
 	int b;
-	long d;
-	double c;
+	long c;
+	double d;
 	float e;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(a));
-	printf("Size of a int: %ld byte(s)\n", sizeof(b));
+	printf("Size of an int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 	printf("Size of a float: %ld byte(s)\n", sizeof(e));
