375fe5ed62314c7ef55aa3e8b635411983333f49
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-17 19:35:07 +0000

    Added file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the dictionary in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	do {
+		for (x = 0; x <= 9; x++)
+		{
+			putchar(x + 48);
+			if (x != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	} while (x < 9);
+	putchar('\n');
+	return (0);
+}
