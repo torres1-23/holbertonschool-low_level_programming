31a2fdff147d947e828b05dbb98d461f19ef39cf
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-17 19:53:53 +0000

    Added file 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the dictionary in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	do {
+		for (x = 0; x <= 99; x++)
+		{
+			putchar(x/10 + 48);
+			putchar(x%10 + 48);
+			if (x != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	} while (x < 99);
+	putchar('\n');
+	return (0);
+}
