384d8f82ae80a6b6f5032bc6921c0e0bef1a8267
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-17 17:19:26 +0000

    Added file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the dictionary in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	do {
+		for (x = ; x <= 9; x++)
+			print(x);
+	} while (x < 9);
+	putchar('\n');
+	return (0);
+}
