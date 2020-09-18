1f98ec82a17bca9d7dbbe2d85d3baa14d8c683c1
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-18 00:23:33 +0000

    Added file 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the number from 00 to 99 separated by a comma
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	do {
+		for (x = 0; x <= 9; x++)
+		{
+				for (y = x + 1; y <= 9; y++)
+				{
+					for (z = y + 1; z <= 9; z++)
+					{
+						putchar(x + 48);
+						putchar(y + 48);
+						putchar(z + 48);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	} while (x < 9);
+	putchar('\n');
+	return (0);
+}
