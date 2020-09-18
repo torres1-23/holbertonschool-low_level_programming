544e0fd9f75f09d4edcc002350e7e801f94ab80a
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-18 09:34:33 -0500

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,20 +15,17 @@ int main(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		for (y = x+1; y <= 9; y++)
 		{
-			for (a = x+1; a <= 9; a++)
+			for (a = y+1; a <= 9; a++)
 			{
-				for (b = y + 1; b <= 9; b++)
+				for (b = a + 1; b <= 9; b++)
 				{
-					if (x < a)
+					if (x < a && y < a)
 					{	
 						putchar(x + 48);
 						putchar(y + 48);
 						putchar(32);
-					}
-					if (y < b)
-					{
 						putchar(a + 48);
 						putchar(b + 48);
 						putchar(44);
