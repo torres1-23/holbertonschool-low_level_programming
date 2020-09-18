70be013d24ec5e7511cbdc86e0fc6a8e2994046d
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-17 19:57:18 -0500

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,29 @@
 
 int main(void)
 {
-	int x;
-	int y;
-	int z;
-	int a;
+int x;
+int y;
+int z;
+int a;
 
-	for (x = 0; x <= 9; x++)
-	{
-		for (y = x + 1; y <= 9; y++)
-		{
-			for (z = y + 1; z <= 9; z++)
-			{
-				putchar(x + 48);
-				putchar(y + 48);
-				putchar(z + 48);
-				a = 100 * x + y * 10 + z;
-				if (a != 789)
-				{
-					putchar(44);
-					putchar(32);
-				}
-			}
-		 }
-	}
-	putchar(10);
-	return (0);
+for (x = 0; x <= 9; x++)
+{
+for (y = x + 1; y <= 9; y++)
+{
+for (z = y + 1; z <= 9; z++)
+{
+putchar(x + 48);
+putchar(y + 48);
+putchar(z + 48);
+a = 100 * x + y * 10 + z;
+if (a != 789)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
 }
