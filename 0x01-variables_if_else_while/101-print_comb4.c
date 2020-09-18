ca15c773d69aba39e7fc82adbf4c8cad28266b80
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-18 00:41:36 +0000

    Added 101 file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,23 +11,26 @@ int main(void)
 	int x;
 	int y;
 	int z;
+	int a;
 
-	do {
-		for (x = 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
 		{
-				for (y = x + 1; y <= 9; y++)
+			for (z = y + 1; z <= 9; z++)
+			{
+				putchar(x + 48);
+				putchar(y + 48);
+				putchar(z + 48);
+				a= 100 * x + y * 10 + z;
+				if (a != 789)
 				{
-					for (z = y + 1; z <= 9; z++)
-					{
-						putchar(x + 48);
-						putchar(y + 48);
-						putchar(z + 48);
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
-		}
-	} while (x < 9);
+			}
+		 }
+	}
 	putchar('\n');
 	return (0);
 }
