6356ba4cadf3f08e77b8f2fdd1efb7a8952678f0
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-18 10:15:38 -0500

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,37 +6,36 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-	int d; 
-	int e;
-	int f;
+int a;
+int b;
+int c;
+int d; 
+int e;
+int f;
 
-	for (a = 0; a < 100; a++)
-	{
-		b = a / 10;
-		c = a % 10;
-		for (d = 0; d < 100; d++)
-		{
-			e = d / 10;
-			f = d % 10;
-
-			if (b < e || (b == e && c < f))
-			{
-				putchar(b + 48);
-				putchar(c + 48);
-				putchar(32);
-				putchar(e + 48);
-				putchar(f + 48);
-				if (!(b == 9 && c == 8))
-				{
-					putchar(44);
-					putchar(32);
-				}
-			}
-		}
-	}
-	putchar(10);
-	return (0);
+for (a = 0; a < 100; a++)
+{
+b = a / 10;
+c = a % 10;
+for (d = 0; d < 100; d++)
+{
+e = d / 10;
+f = d % 10;
+if (b < e || (b == e && c < f))
+{
+putchar(b + 48);
+putchar(c + 48);
+putchar(32);
+putchar(e + 48);
+putchar(f + 48);
+if (!(b == 9 && c == 8))
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
 }
