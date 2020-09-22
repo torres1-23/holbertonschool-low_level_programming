9419a7b3b726887f54bc511aaebc24dd611853c1
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-22 16:27:52 -0500

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,19 +17,26 @@ void print_times_table(int n)
 			for (x = 0; x <= n; x++)
 			{
 				a = y * x;
+				d = a / 100;
 				b = a / 10;
 				c = a % 10;
 				if (x == 0)
 				{
 					_putchar(0 + '0');
 				}
-				else if (b == 0 && x != 0)
+				else if (d == 0 && b == 0 && x != 0)
 				{
 					_putchar(' ');
 					_putchar(c + '0');
 				}
+				else if (d == 0 && b != 0 && x != 0)
+				{
+					_putchar(b + '0');
+					_putchar(c + '0');
+				}
 				else
 				{
+					_putchar(d + '0');
 					_putchar(b + '0');
 					_putchar(c + '0');
 				}
