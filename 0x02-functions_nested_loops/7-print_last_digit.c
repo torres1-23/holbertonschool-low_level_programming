04351c81973e4e00be1371360ead4475a6f0ab27
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-22 10:13:44 -0500

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,15 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0)
+	int a = n % 10;
+	if (a > 0)
 	{
-		n = n % 10;
-		_putchar(n + '0');
+		_putchar(a + '0');
 	}
 	else
 	{
-		n = (n * -1) % 10;
-		_putchar(n + '0');
+		a = a * -1;
+		_putchar(a + '0');
 	}
-	return (n);
+	return (a);
 }
