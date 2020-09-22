25fb5e445928268079099a12d374cc20dda407e2
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-22 10:43:42 -0500

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,11 +15,11 @@ void times_table(void)
 			a = y * x;
 			b = a / 10;
 			c = a % 10;
-			if (b == 0 && c == 0 && x == 0)
+			if (b == 0 && c == 0 && y == 0)
 			{
 				_putchar(0 + '0');
 			}
-			else if (b == 0 && c != 0 && x != 0)
+			else if (b == 0 && c != 0 && y != 0)
 			{
 				_putchar(' ');
 				_putchar(c + '0');
