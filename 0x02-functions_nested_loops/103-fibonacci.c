cf18cd476e99ad60a53283f3ee59670b60766091
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-23 00:17:57 +0000

    Added file 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main- main
+ *
+ *Return: 0 if success
+ */
+
+int main(void)
+{
+	int sum = 0, num = 0, num1 = 0, num2 = 1;
+
+	while (num < 4000001)
+	{
+		num = num1 + num2;
+		num1 = num2;
+	        num2 = num;
+		if (num % 2 == 0)
+			sum = num + sum;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
