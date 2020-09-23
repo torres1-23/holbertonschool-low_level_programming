04abb5df6e9bef720283c411a01205bc9b1449ed
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-23 11:30:20 -0500

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,22 +18,24 @@ int main(void)
 			printf("%lu, ", num);
 		else if (n == 2)
 			printf("%lu, ", num1);
-		else
+		else if (n <= 50)
 		{
 			num2 = num + num1;
 			num = num1;
 			num1 = num2;
-			if (n > 50 && n < 98)
-			{
-				prim = num2 / 1000000;
-				sec = num2 % 1000000;
-				printf("%lu%lu, ", prim, sec);
-			}
-			else if (n <= 50)
-				printf("%lu, ", num2);
-			else
-				printf("%lu\n", num2);
+		else
+		{	
+			prim = num2 / 100000;
+			sec = num2 % 100000;
 		}
+		if (n <= 50 && n > 2)
+			printf("%lu, ", num2);
+		else if (n > 50 && n < 98)
+		{
+			printf("%lu%lu, ", prim, sec);
+		}	
+		else
+			printf("%lu\n", prim, sec);
 	}
 	return (0);
 }
