2a1f9d95de17579c5a788f686bcd2243b86fb395
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-24 22:06:30 -0500

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,15 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else if (i % 5 == 0 && i % 3 != 0)
-			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 5 == 0)
+			if (i != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz\n");
+		else if (i % 3 == 0)
 			printf("Fizz ");
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
 	return (0);
 }
