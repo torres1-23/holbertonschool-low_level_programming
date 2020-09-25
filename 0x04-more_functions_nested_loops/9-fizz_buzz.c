57872531b7fc3757eecb0590440f011e7d67d93a
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-24 21:56:30 -0500

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else if (i % 3 == 0)
