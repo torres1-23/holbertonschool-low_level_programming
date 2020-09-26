d467a1ee114f9b68eafc81851096a5589a73c91e
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-26 18:54:00 -0500

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,10 +9,23 @@ void print_number(int n)
 {
 	unsigned int d;
 
-	if (n < 0)
+	if (n < 0 && n != 2147483648)
 	{
 		_putchar('-');
 		n = -n;
+	}
+	if (n == -2147483648)
+	{	
+		_putchar('-');
+		_putchar((n / 1000000000) + '0');
+		_putchar((n / 100000000) + '0');
+		_putchar((n / 10000000) + '0');
+		_putchar((n / 1000000) + '0');
+		_putchar((n / 100000) + '0');
+		_putchar((n / 10000) + '0');
+		_putchar((n / 1000) + '0');
+		_putchar((n / 100) + '0');
+		_putchar((n % 10) + '0');
 	}
 	d = n / 10;
 	if (d)
