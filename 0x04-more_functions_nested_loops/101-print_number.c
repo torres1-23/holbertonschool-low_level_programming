be5656bcaf9b0f9a9977687a55e090a5d5cc23f4
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-26 02:35:29 +0000

    Added file 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,15 @@
 
 void print_number(int n)
 {
+	unsigned int d
+;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
+	d = n / 10;
+	if (d)
+		print_number(d);
 	_putchar(n % 10 + '0');
 }
