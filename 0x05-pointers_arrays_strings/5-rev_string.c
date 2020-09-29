402e85b24bbaa6c9d4609f6bf1f70d85d62d8b81
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-29 09:40:21 -0500

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,20 +7,16 @@
 
 void rev_string(char *s)
 {
-	int i = 0, j = 0, p = 0;
+	int i = 0, j = 0;
 	int x;
-	char a[10];
+	char a;
 
 	while (s[i] != '\0')
 		i++;
 	for (x = i - 1; x >= 0; x--)
 	{
-		a[j] = s[x];
+		a = s[x];
+		s[j] = a;
 		j++;
-	}
-	for (x = 0; x <= i - 1; x++)
-	{
-		s[p] = a[x];
-		p++;
 	}
 }
