07a0debfa6b9f9374d20fbc9407558885221326c
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-30 04:01:22 +0000

    Added file 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point, generates random valid passwords for the program
+ * 101-crackme.
+ *
+ * Return: 0 if succes
+ */
+
+int main(void)
+{
+	return (0);
+}
