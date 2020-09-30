8ec96f4b78f48815ca48de5c1edd70b9780ff2ca
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-09-29 22:45:06 -0500

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,8 +10,8 @@
 
 int _atoi(char *s)
 {
-	signed int i = 0, length = 0, sum = 0, sign = 0, num = 1;
-	signed int init, finish, d;
+	unsigned int i = 0, length = 0, sum = 0, sign = 0, num = 1;
+	int init, finish, d;
 
 	while (s[length] != '\0')
 	{
