3244f2738667e500d033185d6250f2a79585a040
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-01 22:55:49 +0000

    Added file 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,18 +12,27 @@ char *cap_string(char *a)
 	int i = 0, j = 0;
 	int y;
 	int aft;
-	char s[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	char s[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')',
+		    '{', '}'};
 
 	while (s[j] != '\0')
 		j++;
 	while (a[i] != '\0')
 	{
-		for (y = 0; y < j; y++)
+		if (i == 0)
 		{
-			aft = i + 1;
-			if (a[i] == s[y])
-				if (a[aft] >= 97 && a[aft] <= 122)
-					a[aft] = a[aft] - 32;
+			if (a[i] >= 97 && a[i] <= 122)
+				a[i] = a[i] - 32;
+		}
+		else
+		{
+			for (y = 0; y < j; y++)
+			{
+				aft = i + 1;
+				if (a[i] == s[y])
+					if (a[aft] >= 97 && a[aft] <= 122)
+						a[aft] = a[aft] - 32;
+			}
 		}
 		i++;
 	}
