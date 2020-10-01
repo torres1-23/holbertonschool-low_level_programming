d077ead90a773dbc4a82c6661d77c62c92ee12e2
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-01 23:32:32 +0000

    Added file 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,18 @@
 
 char *leet(char *a)
 {
-	int i = 0, j = 0;
-	int x;
+	int j = 0;
+	int i;
 	char let[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L', '\0'};
 	char sim[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1', '\0'};
 
-	while (let[i] != '\0')
-		i++;
 	while (a[j] != '\0')
 	{
-		for (x = 0; x < i; x++)
+		for (i = 0; let[i] != '\0'; i++)
 		{
-			if (a[j] == let[x])
+			if (a[j] == let[i])
 			{
-				a[j] = sim[x];
+				a[j] = sim[i];
 			}
 		}
 		j++;
