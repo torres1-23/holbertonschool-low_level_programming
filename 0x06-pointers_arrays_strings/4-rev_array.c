d12e72cfe1af5e1395988fb4b417cf862102c9d7
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-01 20:48:01 +0000

    Added file 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: integer to reverse
+ * @n: elements of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n/2; i++)
+	{
+		t = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = t;
+	}
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -52,4 +52,12 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: integer to reverse
+ * @n: elements of array
+ */
+
+void reverse_array(int *a, int n);
+
 #endif /* _HOLBERTON_H_ */
