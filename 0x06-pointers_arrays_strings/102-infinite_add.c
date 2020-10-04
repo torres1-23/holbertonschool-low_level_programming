2c1bb19eb172a6ad758a799e495181e63d720b10
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-04 21:17:16 +0000

    Added file 102

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -12,12 +12,46 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *a;
-	int size;
-	
-	a = *n1; 
-	a = *n2;
-	a = r;
-	size = size_r + 1;
-	return (a);
+	int i = 0, j = 0, p = 0, desb = 0;
+	int num1, num2, a, t, q, sum, sum1;
+
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	i = i - 1;
+	j = j - 1;
+	while (i >= 0 || j >= 0)
+	{
+		num1 = num2 = 0;
+		if (i >= 0)
+			num1 = n1[i--] - '0';
+		if (j >= 0)
+			num2 = n2[j--] - '0';
+		sum1 = num1 + num2 + desb;
+		if (sum1 >= 10)
+			desb = 1;
+		else
+			desb = 0;
+		sum = (sum1 % 10) + '0';
+		r[p] = sum;
+		p++;
+	}
+	if (i == j && sum1 >= 10)
+		r[p] = '1';
+	p++;
+	r[p] = '\0';
+	q = p;
+	p = p - 1;
+	for (a = 0; a <= p; a++)
+	{
+		t = r[p];
+		r[p] = r[a];
+		r[a] = t;
+		p--;
+	}
+	if (size_r > q)
+		return (r);
+	else
+		return (0);
 }
