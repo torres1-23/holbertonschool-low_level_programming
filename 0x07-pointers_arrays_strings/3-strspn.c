efba02b3577c2a973c4bc52b1a8f092894a93a67
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-05 14:36:12 -0500

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,20 +11,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int x, y;
+	unsigned int x, y, eq;
 	unsigned int cont = 0;
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
 		for (y = 0; accept[y] != '\0'; y++)
 		{
+			eq = 0;
 			if (s[x] == accept[y])
 			{
+				eq = 1;
 				cont++;
 				break;
 			}
 		}
-		break;
+		if (eq == 1)
+			continue;
+		else
+			break;
 	}
 	return (cont);
 }
