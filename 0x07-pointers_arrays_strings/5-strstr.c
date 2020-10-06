4f867cbacd4ba70da1fd589a03a0c67f17046bb6
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-06 13:28:49 -0500

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 			if (haystack[i] == needle[j])
 				break;
 		}
-		for (i; i + j < j; i++)
+		for (i; i < j; i++)
 		{
 			if (haystack[i] == needle[j])
 			{
