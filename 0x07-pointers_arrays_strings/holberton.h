7efd5ca58771526c80186926f01ce3b1357f2304
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-06 01:16:20 +0000

    Added file 6

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -76,4 +76,11 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
+/**
+ * print_chessboard -   prints the chessboard.
+ * @a: chessboard.
+ */
+
+void print_chessboard(char (*a)[8]);
+
 #endif /* _HOLBERTON_H_ */
