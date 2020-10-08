c7c643575d124bb7469d38ca7bc159b243e65fbf
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-08 12:08:21 -0500

    Update 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -10,7 +10,7 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (!s1[1] && !s2[1])
+	if (!s1[0] && !s2[0])
 		return (1);
 	else if (s1[0] == s2[0])
 		return (wildcmp(s1 + 1, s2 + 1));
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
-		else if (s1[0] != '\0' && !s2[0])
+		else if (s1[1] && !s2[0] || !s1[0] && s2[1])
 			return (0);
 		else
 			return (0);
