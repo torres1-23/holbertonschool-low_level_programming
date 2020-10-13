b9e9d30775078fade0f23c706a6a52a1b7a32998
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-13 12:07:31 -0500

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	res = malloc(sizeof(char) * size);
-	if (size != 0 || res != NULL)
+	if (size == 0 || res == NULL)
+	{
+		return (NULL);
+	}
+	else
 	{
 		for (i = 0; i < size; i++)
 			res[i] = c;
+		return (res);
 	}
-	else
-		return (NULL);
-	return (res);
+	return (NULL);
 }
