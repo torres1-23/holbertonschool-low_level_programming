839e5c4ea2e3d94da19a2640672aa6952a7da498
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-13 12:55:49 -0500

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,10 +18,12 @@ char *_strdup(char *str)
 	
 	if (str == NULL)
 		return (NULL);
-	copy = malloc(sizeof(char) * sizeof(str));
+	while (str[len] != '\0')
+		len ++;
+	copy = malloc(sizeof(char) * len + 1);
 	if (copy == NULL)
 		return (NULL);
-	for (i = 0; i <= sizeof(str); i++)
+	for (i = 0; i < len; i++)
 		copy[i] = str[i];
 	copy[i] = '\0';
 	return (copy);
