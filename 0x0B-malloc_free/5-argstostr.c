a535eaf728f97380360d5d7f5ebd4a91e4814a25
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-13 19:54:24 -0500

    Update 5-argstostr.c

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -13,7 +13,7 @@
 char *argstostr(int ac, char **av)
 {
 	char *arr;
-	int i, j, p = 0;
+	int i, j, l, p = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
@@ -22,8 +22,10 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		j = 0;
-		for (j = 0; av[i][j]; j++)
+		l = 0;
+		while (av[i][j])
+			l++;
+		for (j = 0; j < l; j++)
 		{
 			arr[p] = av[i][j];
 			p++;
