b688e9faaf2dc83d1488746382bb35da156d7417
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-15 11:43:15 -0500

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size =(max - min) + 1;
+	size = (max - min) + 1;
 	mem = malloc(sizeof(int) * size);
 	if (mem == NULL)
 		return (NULL);
