fdff3572f68e72232e0e37f4cb1ee2b7c168ff52
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-16 03:50:33 +0000

    Added file 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,24 +14,23 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *mem;
-	char *omem = ptr;
+	char *om = ptr;
 	unsigned int i;
 
+	if (new_size == 0 && ptr != NULL)
+        {
+                free(ptr);
+                return (NULL);
+        }
 	if (new_size == old_size)
 		return (ptr);
-	if (omem == NULL)
-		mem = malloc(new_size);
-		return (mem);
-	if (new_size == 0 && omem != NULL)
-	{
-		free(omem);
-		return (NULL);
-	}
 	mem = malloc(new_size);
+	if (ptr == NULL)
+		return (mem);
 	if (mem == NULL)
 		return (NULL);
 	for (i = 0; i < old_size; i++)
-		mem[i] = omem[i];
-	free(omem);
+		mem[i] = om[i];
+	free(om);
 	return (mem);
 }
