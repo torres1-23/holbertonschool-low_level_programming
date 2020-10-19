8c21cef7909e74a049dfb68dd052aecbbc0d71e8
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-19 15:50:12 -0500

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,20 +25,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 		l1++;
 	nm = malloc(sizeof(char) * (l + 1));
 	if (nm == NULL)
+	{
 		free (nm);
 		return (NULL);
+	}
 	ow = malloc(sizeof(char) * (l1 + 1));
 	if (ow == NULL)
+	{
 		free (ow);
 		return (NULL);
+	}
 	for (i = 0; i <= l; i++)
 		nm[i] = name[i];
 	for (j = 0; j <= l1; j++)
 		ow[j] = owner[j];
 	nd = malloc(sizeof(dog_t));
 	if (nd == NULL)
+	{
 		free (nd);
 		return (NULL);
+	}
 	nd->name = nm;
 	nd->age = age;
 	nd->owner = ow;
