81a02210ba98ffe9b6a81c1e07ec4d4a74c5474c
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-19 17:36:34 +0000

    Added file 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+## Structures, typedef repository
+   This repository will contain files that solve tasks involving structures
+   and typedef in C programming language.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog structure
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
