390b9315342006626bf2d693d5ebde7ad3df1b11
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-19 17:54:43 +0000

    Added file 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,16 @@
  * @name: Name of dog
  * @age: Age of dog
  * @owner: Owner of dog.
+ *
+ * Return: 0 in success, 1 if fails.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return (1);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	return (0);
 }
