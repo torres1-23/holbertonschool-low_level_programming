cc158f37c497836da3f97858eab411bceac1db08
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-19 21:51:30 +0000

    Added file 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs.
+ * @d: Structure of dog
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
