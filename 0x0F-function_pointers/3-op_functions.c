4e52cf945e53cf61873495bf82ed10a25b1b0a3b
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-21 16:22:20 -0500

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -10,7 +10,7 @@
 
 int op_add(int a, int b)
 {
-	return ((a) + (b));
+	return (a + b);
 }
 
 /**
@@ -23,7 +23,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-	return ((a) - (b));
+	return (a - b);
 }
 
 /**
@@ -36,7 +36,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	return ((a) * (b));
+	return (a * b);
 }
 
 /**
@@ -49,7 +49,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return ((a) / (b));
+	return (a / b);
 }
 
 /**
@@ -62,5 +62,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	return ((a) % (b));
+	return (a % b);
 }
