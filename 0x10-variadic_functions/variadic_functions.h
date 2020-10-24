ac05d170aeb1bb2f956c91ebb50a78b3b1e3b91e
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-24 14:58:05 -0500

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,23 +6,19 @@
 #include <stdarg.h>
 
 /**
- * struct _type - structure for types.
+ * struct format_s - structure for types.
  * @tp: string type of character.
  * @f: pointer to function that prints.
  */
 
-typedef struct _type
+typedef struct format_s
 {
 	char tp;
 	void (*f)();
-} _type_t;
+} format_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void p_char(va_list a);
-void p_int(va_list a);
-void p_float(va_list a);
-void p_string(va_list a);
 
 #endif /* _VARIADIC_FUNCTIONS_H_ */
