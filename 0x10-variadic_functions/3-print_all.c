04db8941aace0ce2757fee6700c96c64350aa828
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-24 14:17:06 -0500

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 	};
 	va_list alist;
 	char *spc = "";
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, j;
 
 	va_start(alist, format);
 	while (format[i] && format)
