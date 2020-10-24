7aaf1f4f0c9db39527c90e1e8b7c1a20aca31029
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-24 15:14:33 -0500

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,44 +1,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything.
- * @format: list of types of arguments passed to the function
- */
-
-void print_all(const char * const format, ...)
-{
-	type_t types[] = {
-		{'c', p_char},
-		{'i', p_int},
-		{'f', p_float},
-		{'s', p_string},
-		{'\0', '\0'}
-	};
-	va_list alist;
-	char *spc = "";
-	int i = 0, j = 0;
-
-	va_start(alist, format);
-	while (format[i] && format)
-	{
-		j = 0;
-		while (types[j].tp)
-		{
-			if (format[i] == types[j].tp)
-			{
-				printf("%s", spc);
-				types[j].f(alist);
-				spc = ", ";
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(alist);
-}
-
-/**
  * p_char - prints a character
  * @a: character to be printed
  */
@@ -80,4 +42,42 @@ void p_string(va_list a)
 	if (str == NULL)
 		str = "(nil)";
 	printf("%s", str);
+}
+
+/**
+ * print_all - prints anything.
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	type_t types[] = {
+		{'c', p_char},
+		{'i', p_int},
+		{'f', p_float},
+		{'s', p_string},
+		{'\0', '\0'}
+	};
+	va_list alist;
+	char *spc = "";
+	int i = 0, j = 0;
+
+	va_start(alist, format);
+	while (format[i] && format)
+	{
+		j = 0;
+		while (types[j].tp)
+		{
+			if (format[i] == types[j].tp)
+			{
+				printf("%s", spc);
+				types[j].f(alist);
+				spc = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(alist);
 }
