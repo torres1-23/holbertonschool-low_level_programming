61879db8d8a3dafad736c633cc516b364e684fb3
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-29 23:51:19 +0000

    Added file 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * beforemain - prints something before the main function.
+ */
+
+void beforemain(void)
+{
+	printf("You're beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
+}
