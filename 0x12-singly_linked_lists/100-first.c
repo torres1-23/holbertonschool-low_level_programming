4964dc632d3a1dc643c6a6ffbf9352ca072d142e
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-30 08:58:23 -0500

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-
-void beforemain(void) __attribute__ ((constructor));
+#include "lists.h"
 
 /**
  * beforemain - prints something before the main function.
@@ -8,6 +6,6 @@ void beforemain(void) __attribute__ ((constructor));
 
 void beforemain(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
