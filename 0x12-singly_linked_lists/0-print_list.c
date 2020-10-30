e730835efebedf7a1f5faa29f26680d41ab1be21
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-10-30 09:04:14 -0500

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,17 @@ size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
 
-	if (!h)
-		return (0);
-	while (h)
+	if (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		i++;
+		while (h)
+		{
+			if (!h->str)
+				printf("[0] (nil)\n");
+			else
+				printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			i++;
+		}
 	}
 	return (i);
 }
