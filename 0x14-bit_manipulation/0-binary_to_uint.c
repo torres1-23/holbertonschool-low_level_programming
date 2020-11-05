3d6451ccbd9e7fd693f01f26ed3caa4a1ab10561
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-05 12:30:17 -0500

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,20 +12,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0, i = 0, len = 0;
 
-	if (b != NULL)
+	if (b == NULL)
+		return (0);
+	while (b[len])
 	{
-		while (b[len])
-		{
-			if (b[len] < '0' || b[len] > '1')
-				return (0);
-			len++;
-		}
-		dec += b[i] - '0';
-		for (i = 1; i < len; i++)
-		{
-			dec = (dec * 2) + (b[i] - '0');
-		}
-		return (dec);
+		if (b[len] < '0' || b[len] > '1')
+			return (0);
+		len++;
 	}
-	return (0);
+	dec += b[i] - '0';
+	for (i = 1; i < len; i++)
+	{
+		dec = (dec * 2) + (b[i] - '0');
+	}
+	return (dec);
 }
