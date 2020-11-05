f0f786571dc6a8873a674a830c64117c5f913847
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-05 12:12:56 -0500

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,12 +15,14 @@ unsigned int binary_to_uint(const char *b)
 	if (b)
 	{
 		while (b[len])
+		{
+			if (b[len] < '0' || b[len] > '1')
+				return (0);
 			len++;
+		}
 		dec += b[i] - '0';
 		for (i = 1; i < len; i++)
 		{
-			if (b[i] < '0' || b[i] > '1')
-				return (0);
 			dec = (dec * 2) + (b[i] - '0');
 		}
 		return (dec);
