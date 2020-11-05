9f69816f1ce4b316ac88b3d417d4edff5cb60700
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-05 13:13:09 -0500

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -18,13 +18,10 @@ int get_bit(unsigned long int n, unsigned int index)
 		{
 			n = n >> 1;
 		}
-		if (n)
-		{
-			if (n & 1)
-				return (1);
-			else
-				return (0);
-		}
+		if (n & 1)
+			return (1);
+		else
+			return (0);
 	}
 	return (-1);
 }
