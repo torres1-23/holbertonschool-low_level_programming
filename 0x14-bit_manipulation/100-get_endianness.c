e56cd594278a025f9c820a8199f3d5efac7dc8d1
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-05 21:36:52 +0000

    Added file 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	int x = 1;
+	char *y = (char*)&x;
+
+	if (*y == 1)
+		return 1;
+	return (0);
+}
