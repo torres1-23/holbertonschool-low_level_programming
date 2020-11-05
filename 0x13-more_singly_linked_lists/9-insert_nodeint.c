d336a2ab2ab7310a5218aa62baf589e27536643b
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-04 22:49:18 -0500

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,20 +21,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (!new)
 			return (NULL);
 		new->n = n;
-		if (idx == 0)
-		{
-			new->next = *head;
-			*head = new;
-			return (new);
-		}
 		for (i = 1; i < idx; i++)
 		{
 			tmp = tmp->next;
 			if (!tmp)
 				return (NULL);
 		}
-		new->next = tmp->next;
-		tmp->next = new;
+		if (idx == 0)
+		{
+			new->next = *head;
+			*head = new;
+		}
+		else
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+		}
 		return (new);
 	}
 	return (NULL);
