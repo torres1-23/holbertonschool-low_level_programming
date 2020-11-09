c5aa2275fe0886c9d6cc14e8120f9212f828a835
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-09 23:24:02 +0000

    Added file 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,109 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point, copies the content of a file to another file.
+ * @ac: Number of arguments.
+ * @av: Array of arguments.
+ *
+ * Return: 0 in succes.
+ */
+
+int main(int ac, char *av[])
+{
+	int ffrom, fto, bytesrd = 1, byteswr, tmp;
+	char *buffer;
+
+	err_argc(ac);
+	ffrom = open(av[1], O_RDONLY);
+	err_read(ffrom, av[1], NULL);
+	fto = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 436);
+	err_wr(fto, av[2], NULL);
+	buffer = malloc(1024);
+	if (!buffer)
+		return (-1);
+	while (bytesrd != 0)
+	{
+		bytesrd = read(ffrom, buffer, 1024);
+		err_read(bytesrd, av[1], buffer);
+		byteswr = write(fto, buffer, bytesrd);
+		err_wr(byteswr, av[2], buffer);
+		if (byteswr == 1024)
+		{
+			free(buffer);
+			buffer = malloc(1024);
+			if (!buffer)
+				return (-1);
+		}
+	}
+	tmp = close(ffrom);
+	err_close(tmp, ffrom, buffer);
+	tmp = close(fto);
+	err_close(tmp, fto, buffer);
+	free(buffer);
+	return (0);
+}
+
+/**
+ * err_argc - Handles number of arguments error.
+ * @code: code of error.
+ */
+
+void err_argc(int code)
+{
+	if (code == -1)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+ * err_read - handles error from from_file reading.
+ * @code: code of error.
+ * @str: Name of file.
+ * @buffer: Buffer to free.
+ */
+
+void err_read(int code, char *str, char *buffer)
+{
+	if (code == -1)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+		exit(98);
+	}
+}
+
+/**
+ * err_wr - handles write error.
+ * @code: code of error.
+ * @str: name of file.
+ * @buffer: buffer to free.
+ */
+
+void err_wr(int code, char *str, char *buffer)
+{
+	if (code == -1)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+		exit(99);
+	}
+}
+
+/**
+ * err_close - handles close error.
+ * @code: error code.
+ * @str: File name.
+ * @buffer: buffer to free.
+ */
+
+void err_close(int code, int fd, char *buffer)
+{
+	if (code == -1)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		exit(100);
+	}
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -11,5 +11,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void err_argc(int code);
+void err_read(int code, char *str, char *buffer);
+void err_wr(int code, char *str, char *buffer);
+void err_close(int code, int fd, char *buffer);
 #endif /* _HOLBERTON_H_ */
