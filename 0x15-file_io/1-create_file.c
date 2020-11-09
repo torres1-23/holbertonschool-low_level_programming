af8d993a75150642a2035cfe4be1446014cd2047
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-09 12:34:21 -0500

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 384);
 		if (file == -1)
 			return (-1);
-		if (text_content[len])
+		if (text_content && text_content[len])
 		{
 			while (text_content[len])
 				len++;
