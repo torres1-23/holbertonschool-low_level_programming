70fdab676385ff2911f8b9b405875a6625a6302a
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-09 12:24:05 -0500

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,14 +19,12 @@ int create_file(const char *filename, char *text_content)
 		file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 384);
 		if (file == -1)
 			return (-1);
-		if (!text_content)
+		if (text_content)
 		{
-			close(file);
-			return (1);
+			byteswrt = write(file, text_content, len);
+			if (byteswrt == -1)
+				return (-1);
 		}
-		byteswrt = write(file, text_content, len);
-		if (byteswrt == -1)
-			return (-1);
 		close(file);
 		return (1);
 	}
