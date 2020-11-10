73d690bd80c1a47971e3ad3ace89469f08cb9c14
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-11-09 22:25:27 -0500

    Update holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void err_argc(int code);
-void err_read(int code, char *str);
+void err_read(int code, char *str, char *buffer);
 void err_wr(int code, char *str, char *buffer);
 void err_close(int code, int fd, char *buffer);
 #endif /* _HOLBERTON_H_ */
