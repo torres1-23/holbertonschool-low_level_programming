b6fcfd1f684bacccab27baec2c5f5007f4818212
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-14 16:06:55 +0000

    Modified several files

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list.
+ * @head: pointer to list head.
+ *
+ * Return: a pointer to the first node of the reversed list.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	list
+		
+}
