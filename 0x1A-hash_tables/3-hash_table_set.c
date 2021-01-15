c7c7aef3e0e20a00271fced0286385301cee67aa
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-15 09:12:05 -0500

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *head_copy = NULL;
 	unsigned long int index;
 
-	if (!key || !ht || key == '\0')
+	if (!key || !ht || key == '\0' || !value)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
 	if (ht->array[index])
