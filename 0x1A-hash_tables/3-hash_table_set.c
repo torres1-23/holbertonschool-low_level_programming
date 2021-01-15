a4c49d6c0cda005648f4bf4a79c5cd11e607fe32
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-15 01:43:16 +0000

    Add solution to task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: pointer to hash table to add/update key/value.
+ * @key: key to add/update
+ * @value: value to add/update.
+ *
+ * Return: 1 if success, 0 if failure.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL, *head_copy = NULL;
+	unsigned long int index;
+
+	if (!key || !ht)
+		return (0);
+	index = key_index((const unsigned char *) key, ht->size);
+	if (ht->array[index])
+	{
+		head_copy = ht->array[index];
+		while (head_copy)
+		{
+			if (strcmp(head_copy->key, key) == 0)
+			{
+				free(head_copy->value);
+				head_copy->value = strdup(value);
+				if (head_copy->value)
+					return (0);
+				return (1);
+			}
+			head_copy = head_copy->next;
+		}
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = (char *) key;
+	new_node->value = strdup(value);
+	if (!new_node->value)
+		return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
