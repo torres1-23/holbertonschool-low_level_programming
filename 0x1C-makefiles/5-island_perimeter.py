53ace06aa32be6cae04151d6aeb9a52cc81a5d27
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-02-19 23:43:33 +0000

    Add solution to Island Perimeter problem

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""This module defines a function "def island_perimeter(grid)".
+
+Usage:
+    "island_perimeter(...) is used to return the perimeter of the island
+    described in grid:
+
+grid is a list of list of integers:
+    * 0 represents a water zone
+    * 1 represents a land zone
+    * One cell is a square with side length 1
+    * Grid cells are connected horizontally/vertically (not diagonally).
+    * Grid is rectangular, width and height don’t exceed 100
+    * Grid is completely surrounded by water, and there is one island (or
+    nothing).
+The island doesn’t have “lakes” (water inside that isn’t connected to the
+water around the island).
+"""
+
+
+def island_perimeter(grid):
+    """Computes the perimeter of an island.
+
+    Args:
+        grid (list): contains the greed of an island.
+
+    Return:
+        The perimeter of the island.
+    """
+    height = len(grid)
+    width = len(grid[0])
+    perimeter = 0
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if (grid[i][j - 1] == 1):
+                    perimeter -= 2
+                if (grid[i - 1][j] == 1):
+                    perimeter -= 2
+    return perimeter
