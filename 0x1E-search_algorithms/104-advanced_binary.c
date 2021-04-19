f21ac00feac8203b6eff3a26a87f6091893f32ef
torres1-23 <alejo.torres1247@gmail.com>
2021-04-19 21:44:25 +0000

    Add advnced search algorithm function

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in an array of integers
+ * using the Binary search algorithm.
+ * @array: pointer to the first element of the array to search in.
+ * @size: number of elements in array.
+ * @value: value to search for.
+ * Return: first index where value is located, or -1 on error.
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array != NULL)
+		return (binary_search_rec(array, 0, size - 1, value));
+	return (-1);
+}
+
+/**
+ * binary_search_rec - searches for a value in an array of integers
+ * using the Binary search algorithm.
+ * @array: pointer to the first element of the array to search in.
+ * @low: lower index to search.
+ * @high: higher index to search.
+ * @value: value to search for.
+ * Return: index where value is located, or -1 on error.
+ */
+
+int binary_search_rec(int *array, size_t low, size_t high, int value)
+{
+	size_t mid, i;
+
+	if (low < high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[high]);
+		mid = (low + high) / 2;
+		if (array[mid] == value && (mid == 0 || array[mid - 1] != value))
+			return (mid);
+		if (array[mid] >= value)
+			return (binary_search_rec(array, low, mid, value));
+		if (array[mid] < value)
+			return (binary_search_rec(array, mid + 1, high, value));
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search_exp(int *array, int low, int high, int value);
+int advanced_binary(int *array, size_t size, int value);
+int binary_search_rec(int *array, size_t low, size_t high, int value);
 
 #endif /* _SEARCH_ALG_H_ */
