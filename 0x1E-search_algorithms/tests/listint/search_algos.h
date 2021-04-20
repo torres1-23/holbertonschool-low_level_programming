91eac5e850edd99716d40b986a99fd4eda6c57d5
torres1-23 <alejo.torres1247@gmail.com>
2021-04-20 02:16:30 +0000

    Add tests and auxiliary functions directory

---
+++ 0x1E-search_algorithms/tests/listint/search_algos.h
@@ -0,0 +1,36 @@
+#ifndef _SEARCH_ALG_H_
+#define _SEARCH_ALG_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search_exp(int *array, int low, int high, int value);
+int advanced_binary(int *array, size_t size, int value);
+int binary_search_rec(int *array, size_t low, size_t high, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+#endif /* _SEARCH_ALG_H_ */
