d89cb44b0a2e56410e4c24a4b252d2a3ab2a566f
torres1-23 <alejo.torres1247@gmail.com>
2021-04-20 17:52:12 +0000

    Update conditional

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	unsigned int step = 0, prev = 0, i;
 
-	if (array != NULL)
+	if (array != NULL || size == 0)
 	{
 		while (array[step] < value && step < size)
 		{
